ab8ea27a6a23f78f67c20b2a9302ae59979d7d81
test <koush0811@gmail.com>
2026-01-19 18:14:53 +0900

    scrolle実装

---
+++ script.js
@@ -0,0 +1,23 @@
+const topel = document.querySelector('.top');
+const btns = document.querySelectorAll('.btn')
+const logo = document.querySelector('.logo')
+const none = document.querySelector('.none')
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 1){
+        topel.classList.add('scrolled')
+        logo.classList.add('scrolled')
+        none.classList.add('scrolled')
+        btns.forEach(btn => {
+            btn.classList.add('scrolled')
+        })
+        
+    }else{
+        topel.classList.remove('scrolled')
+        logo.classList.remove('scrolled')
+        none.classList.remove('scrolled')
+        btns.forEach(btn => {
+            btn.classList.remove('scrolled')
+        })
+    }
+})
