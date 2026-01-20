cf3c09eccd92ec829ff4b26df0435d05a9252fe3
koush811 <koush0811@gmail.com>
2026-01-20 23:54:13 +0900

    fieldの途中

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 const topel = document.querySelector('.top');
-const btns = document.querySelectorAll('.btn')
-const logo = document.querySelector('.logo')
 const none = document.querySelector('.none')
 
 window.addEventListener('scroll',()=>{
     if(window.scrollY > 1){
         topel.classList.add('scrolled')
-        logo.classList.add('scrolled')
         none.classList.add('scrolled')
-        btns.forEach(btn => {
-            btn.classList.add('scrolled')
-        })
         
     }else{
         topel.classList.remove('scrolled')
-        logo.classList.remove('scrolled')
         none.classList.remove('scrolled')
-        btns.forEach(btn => {
-            btn.classList.remove('scrolled')
-        })
     }
 })
 
 
 const content = document.querySelector('.mission-content')
+const wrap = document.querySelector('.wrap')
 function Open(){
     content.classList.add('open')
+    wrap.classList.add('open')
 }
 function Close(){
     content.classList.remove('open')
+    wrap.classList.remove('open')
 }
 
 
