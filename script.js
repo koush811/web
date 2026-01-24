96487c8033fc14fffb82de7552ec0ab04144917f
koush811 <koush0811@gmail.com>
2026-01-24 09:51:42 +0900

    formのjs実装

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const topel = document.querySelector('.top');
 const none = document.querySelector('.none')
 
-window.addEventListener('scroll',updateheader)
-window.addEventListener('load',updateheader)
+window.addEventListener('scroll', updateheader)
+window.addEventListener('load', updateheader)
 
-function updateheader(){
-    if(window.scrollY > 0){
+function updateheader() {
+    if (window.scrollY > 0) {
         topel.classList.add('scrolled')
         none.classList.add('scrolled')
-        
-    }else{
+
+    } else {
         topel.classList.remove('scrolled')
         none.classList.remove('scrolled')
     }
@@ -19,16 +19,38 @@ function updateheader(){
 const content = document.querySelector('.mission-content')
 const wrap = document.querySelector('.wrap')
 const body = document.querySelector('body')
-function Open(){
+function Open() {
     content.classList.add('open')
     wrap.classList.add('open')
     body.style.overflow = "hidden"
 }
-function Close(){
+function Close() {
     content.classList.remove('open')
     wrap.classList.remove('open')
     body.style.overflow = ""
 }
+
+const inputs = document.querySelectorAll('input')
+const formbtn = document.querySelector('.formbtn')
+const adress = document.getElementById('adress')
+
+formbtn.addEventListener('click', () => {
+    inputs.forEach(input => {
+        if (input.value === "") {
+            input.style.borderColor = "red"
+        } else {
+            input.style.borderColor = "rgb(187, 185, 185)"
+        }
+    });
+    if (!adress.value.includes('@')) {
+        console.log("none")
+        adress.style.borderColor = "red"
+
+    }
+})
+
+
+
 
 
 
